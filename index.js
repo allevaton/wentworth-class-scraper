a74685bc5d897d0c0e43f789e5d173dcfc704016
Nick Allevato <nicholas.allevato@gmail.com>
2016-08-09 01:16:52 -0400

    Authenticator is up and running

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+'use strict';
+
+const fs = require('fs');
+const LeopardWebAuthenticator = require('./src/authenticator');
+
 if (require.main === module) {
-  console.log('called directly');
+  fs.readFile('creds', (e, data) => {
+    let [username, password] = data.toString().split('\n');
+    let authenticator = new LeopardWebAuthenticator(username, password)
+    authenticator.authenticate()
+      .then(() => {
+        authenticator.get('https://prodweb2.wit.edu/SSBPROD/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu', (err, response, body) => {
+          debugger;
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+
 }
 else {
-  console.log('required');
+  module.exports = {
+    LeopardWebAuthenticator
+  }
 }
---
+++ src/authenticator.js
@@ -0,0 +1,77 @@
+const url = require('url');
+
+const request = require('request');
+const cheerio = require('cheerio');
+
+const loginUrl = 'https://prodweb2.wit.edu/ssomanager/c/SSB';
+const authenticateUrl = 'https://cas.wit.edu';
+const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36';
+
+module.exports = class LeopardWebAuthenticator {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+
+    this.cookieJar = request.jar();
+    this.request = request.defaults({
+      jar: this.cookieJar,
+      headers: {
+        'User-Agent': userAgent
+      },
+      gzip: true
+    });
+
+    this.get = this.request.get
+    this.post = this.request.post;
+    this.put = this.request.put;
+    this._authenticated = false;
+  }
+
+  get authenticated() {
+    return this._authenticated;
+  }
+
+  authenticate(username = this.username, password = this.password) {
+    return new Promise((resolve, reject) => {
+      this.request(loginUrl, (err, response, body) => {
+        if (err)
+          reject(err);
+
+        let $ = cheerio.load(body);
+        let form = $('form#fm1');
+
+        let formData = { username, password };
+
+        form.find('input[type=hidden]').each((i, hidden) => {
+          // Fill the cookie jar with these hidden form elements
+          let cookie = request.cookie(`${hidden.attribs.name}=${hidden.attribs.value}; Domain=wit.edu`);
+          formData[hidden.attribs.name] = hidden.attribs.value;
+          this.cookieJar.setCookie(cookie, authenticateUrl);
+        });
+
+        // TODO find out why Request is not properly following redirects
+        let postUrl = url.resolve(authenticateUrl, form[0].attribs.action);
+        this.request.post({
+          url: postUrl,
+          form: formData
+        }, (err, response, body) => {
+          if (err)
+            reject(err);
+
+          if (response.statusCode >= 300 && response.statusCode < 400) {
+            // Follow the redirect manually, this cements the authentication
+            this.request.get(response.headers.location, (err, response, body) => {
+              if (err)
+                reject(err);
+
+              this._authenticated = true;
+              resolve();
+            });
+          }
+          else
+            reject('Incorrect login');
+        });
+      });
+    });
+  }
+}
